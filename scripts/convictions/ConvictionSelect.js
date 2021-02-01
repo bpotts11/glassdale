aa1522a6f2d94044482fed89ddeb867c64ce8a19
bpotts11 <brittneympotts@gmail.com>
2021-02-01 03:21:52 -0600

    added feature to filter criminals by their concictions

--- scripts/convictions/ConvictionSelect.js
+++ scripts/convictions/ConvictionSelect.js
@@ -1,6 +1,28 @@
 import { getConvictions, useConvictions } from "./ConvictionProvider.js";
 
+/*
+Which element in your HTML contains all components?
+That's your Event Hub. Get a reference to it here.
+*/
+const eventHub = document.querySelector(".container")
 const contentTarget = document.querySelector(".filters__crime")
+
+// On the event hub, listen for a "change" event.
+eventHub.addEventListener("change", event => {
+
+    // Only do this if the `crimeSelect` element was changed
+    if (event.target.id === "crimeSelect") {
+        // Create custom event. Provide an appropriate name.
+        const customEvent = new CustomEvent("crimeChosen", {
+            detail: {
+                crimeThatWasChosen: event.target.value
+            }
+        })
+
+        // Dispatch to event hub
+        eventHub.dispatchEvent(customEvent)
+    }
+})
 
 export const ConvictionSelect = () => {
     getConvictions()
