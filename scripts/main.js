64fc799fd05c202cc855637fc214af62163e11d7
bpotts11 <brittneympotts@gmail.com>
2021-02-01 15:14:23 -0600

    completed feature for notes to be saved to the local api and are able to be fetched back and dispayed on the dom.

--- scripts/main.js
+++ scripts/main.js
@@ -3,9 +3,13 @@ import { CriminalList } from "./criminals/CriminalList.js";
 import { OfficerList } from "./officers/OfficerList.js";
 import { OfficerSelect } from "./officers/OfficerSelect.js";
 import { NoteForm } from "./notes/NoteForm.js";
+import { ShowNoteButton } from "./notes/ShowNotesButton.js";
+import "./notes/NoteList.js"
 
 CriminalList()
 ConvictionSelect()
 OfficerList()
 OfficerSelect()
 NoteForm()
+ShowNoteButton()
+
--- scripts/notes/NoteForm.js
+++ scripts/notes/NoteForm.js
@@ -1,35 +1,31 @@
-import { saveNote } from "./NoteDataProvider.js"
+import { saveNote } from "./NoteProvider.js"
 
 const contentTarget = document.querySelector(".noteFormContainer")
 const eventHub = document.querySelector(".container")
 
 const render = () => {
     contentTarget.innerHTML = `
-        <form action="" id="noteForm">
-
         <div class="date">
             <label for="noteForm--date">Date</label>
-            <input type="date" id="noteForm--date">
+            <input type="date" id="note-date">
         </div>
         
         <div class="suspect">
             <label for="noteForm--suspect">Suspect:</label>
-            <input type="text" id="noteForm--suspect"></input>
+            <input type="text" id="note-suspect"></input>
         </div>
         
         <div class="text">
             <label for="noteForm--text">Note:</label>
-            <input type="text" id="noteForm--text"></input>
+            <input type="text" id="note-text"></input>
         </div>
 
         <div class="author">
             <label for="noteForm--author">Author:</label>
-            <input type="text" id="noteForm--Author"></input>
+            <input type="text" id="note-author"></input>
         </div>
             
             <button id="saveNote">Save Note</button>
-        </form>
-        
     `
 }
 
@@ -40,10 +36,10 @@ export const NoteForm = () => {
 // Handle browser-generated click event in component
 eventHub.addEventListener("click", clickEvent => {
     if (clickEvent.target.id === "saveNote") {
-        const date = document.querySelector("#noteForm--date").value
-        const suspect = document.querySelector("#noteForm--suspect").value
-        const text = document.querySelector("#noteForm--text").value
-        const author = document.querySelector("#noteForm--author").value
+        const date = document.querySelector("#note-date").value
+        const suspect = document.querySelector("#note-suspect").value
+        const text = document.querySelector("#note-text").value
+        const author = document.querySelector("#note-author").value
 
         // Make a new object representation of a note
         const newNote = {
@@ -52,14 +48,9 @@ eventHub.addEventListener("click", clickEvent => {
             "suspect": suspect,
             "text": text,
             "author": author
-
         }
 
         // Change API state and application state
         saveNote(newNote)
     }
 })
-
-// const NoteForm = () => {
-//     // rest of the code here
-// }
